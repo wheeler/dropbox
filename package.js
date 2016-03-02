c52228173b5fa2b9e77553f7a657a33002b4d373
Vivek Dinesh <vivekdinesh5@gmail.com>
2016-03-02 20:41:16 +0530

    Version constraints

--- package.js
+++ package.js
@@ -7,6 +7,8 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
+  api.versionsFrom("METEOR@1.2.0.2");
+  
   api.use('oauth2', ['client', 'server']);
   api.use('oauth', ['client', 'server']);
   api.use('http', ['server']);
